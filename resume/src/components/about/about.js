16d1cd6efe75c7ae52a6830c74acc564f33f620f
Donavan Duplessis <donavan.duplessis@outlook.com>
2022-02-25 13:45:00 -0500

    Created skeleton

---
+++ resume/src/components/about/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+      <about id="home">
+        ....
+      </about>
+      </React.Fragment>
+    );
+  }
+}
